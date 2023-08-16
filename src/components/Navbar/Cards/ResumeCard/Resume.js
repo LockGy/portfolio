b8031c36d5cbb2912b0f47b1b4090b981d4b3aae
LockGy <oggyyear123@gmail.com>
2023-08-16 14:57:47 +0700

    resume update

--- src/components/Navbar/Cards/ResumeCard/Resume.js
+++ src/components/Navbar/Cards/ResumeCard/Resume.js
@@ -1,10 +1,119 @@
-import react from "react";
 import './Resume.css';
+import Line from './Img/Line 5.svg'
+import Education_icon from './Img/education_icon.svg'
+import Resume_icon from './Img/resume_icon.svg'
 
 export default function Resume(){
 
     return(
         <div className="Resume">
+            <div className='Resume-text'>
+                <h1>Resume</h1>
+                <img className='Line' src={Line} alt='Line'></img>
+                <hr/>
+            </div>
+
+            <div className="Resume-detail">
+                <div className="education">
+                    <div className="education-title">
+                        <img src={Education_icon} alt=""></img>
+                        <h2 className="Block-text">Education</h2>
+                    </div>
+
+                    <div className="block1">
+                        <p className="time">2020-2021</p>
+                        <p className="major">Software Development</p>
+                        <p className="College">Moringa School</p>
+                    </div>
+
+                    <div className="block2">
+                        <p className="time">2012-2016</p>
+                        <p className="major">Disaster Management</p>
+                        <p className="College">Masinde Muliro Universiity </p>                        
+                    </div>
+                </div>
+
+                <div className="experience">
+                    <div className="experience-title">
+                        <img src={Resume_icon} alt=""></img>
+                        <h2 className="Block-text">Resume</h2>
+                    </div>
+
+                    <div className="block2">
+                        <p className="time">2022 - Present</p>
+                        <p className="major">Technical Mentor</p>
+                        <p className="College">Moringa School</p>
+                    </div>
+
+                    <div className="block1">
+                        <p className="time">2021-2022</p>
+                        <p className="major">Website Development </p>
+                        <p className="College">Village 2 Nation</p>                        
+                    </div>
+                </div>
+
+                <div className="work-skills">
+                    <p className="work-skills-title">Work Skills</p>
+                    <div className='work-skills-block'>
+                        <div className='work-skills-block-information'>
+                            <p className='work-skills-block-information-text'>NEXT.js</p>
+                        </div>
+
+                        <div className='work-skills-block-information'>
+                            <p className='work-skills-block-information-text'>React.js</p>
+                        </div>
+
+                        <div className='work-skills-block-information'>
+                            <p className='work-skills-block-information-text'>HTML5</p>
+                        </div>
+
+                        <div className='work-skills-block-information'>
+                            <p className='work-skills-block-information-text'>CSS3</p>
+                        </div>
+
+                        <div className='work-skills-block-information'>
+                            <p className='work-skills-block-information-text'>Tailwind CSS</p>
+                        </div>
+
+                        <div className='work-skills-block-information'>
+                            <p className='work-skills-block-information-text'>Figma</p>
+                        </div>
+
+                        <div className='work-skills-block-information'>
+                            <p className='work-skills-block-information-text'>JavaScript</p>
+                        </div>
+
+                        <div className='work-skills-block-information'>
+                            <p className='work-skills-block-information-text'>Mongo DB</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="soft-skills">
+                    <p className="soft-skills-title">Soft Skills</p>
+                    <div className='soft-skills-block'>
+                        <div className='soft-skills-block-information'>
+                            <p className='soft-skills-block-information-text'>Time Management</p>
+                        </div>
+
+                        <div className='soft-skills-block-information'>
+                            <p className='soft-skills-block-information-text'>Mentorship</p>
+                        </div>
+
+                        <div className='soft-skills-block-information'>
+                            <p className='soft-skills-block-information-text'>Impeccable Communication</p>
+                        </div>
+
+                        <div className='soft-skills-block-information'>
+                            <p className='soft-skills-block-information-text'>Flexibility</p>
+                        </div>
+
+                        <div className='soft-skills-block-information'>
+                            <p className='soft-skills-block-information-text'>Research</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,8 +2,9 @@ import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import {BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import Home from './Cards/HomeCard/Home'
+import Home from './Cards/HomeCard/Home';
 import Resume from "./Cards/ResumeCard/Resume";
+import Work from './Cards/WorkCard/Work'
 
 export default function Navbar(){
     const [buttonStates, setButtonStates] = useState({
@@ -28,7 +29,7 @@ export default function Navbar(){
                 <ul>
                     <li>
                         <Link to='/Home'>
-                            <a href="#" className={buttonStates.home? 'active-button':'inactive-button'} onClick={()=>toggleButton('home')}>
+                            <a className={buttonStates.home? 'active-button':'inactive-button'} onClick={()=>toggleButton('home')}>
                                 <div className="icon_box">
                                     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
                                     <span class="material-symbols-outlined">home</span>
@@ -42,7 +43,7 @@ export default function Navbar(){
 
                     <li>
                         <Link to='/Resume'>
-                            <a href="#" className={buttonStates.resume? 'active-button':'inactive-button'} onClick={()=>toggleButton('resume')}>
+                            <a className={buttonStates.resume? 'active-button':'inactive-button'} onClick={()=>toggleButton('resume')}>
                                 <div className="icon_box">
                                     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
                                     <span class="material-symbols-outlined">assignment</span>
@@ -54,18 +55,20 @@ export default function Navbar(){
                     </li>
 
                     <li>
-                        <a href="#" className={buttonStates.work? 'active-button':'inactive-button'} onClick={()=>toggleButton('work')}>
-                            <div className="icon_box">
-                                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-                                <span class="material-symbols-outlined">work</span>
-                                <p className="text">Work</p>
-                            </div>                        
-                        </a>
+                        <Link to='/Work'>
+                            <a className={buttonStates.work? 'active-button':'inactive-button'} onClick={()=>toggleButton('work')}>
+                                <div className="icon_box">
+                                    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+                                    <span class="material-symbols-outlined">work</span>
+                                    <p className="text">Work</p>
+                                </div>                        
+                            </a>                            
+                        </Link>
 
                     </li>
 
                     <li>
-                        <a href="#" className={buttonStates.contacts? 'active-button':'inactive-button'} onClick={()=>toggleButton('contacts')}>
+                        <a className={buttonStates.contacts? 'active-button':'inactive-button'} onClick={()=>toggleButton('contacts')}>
                             <div className="icon_box">
                                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
                                 <span class="material-symbols-outlined">contacts</span>
@@ -79,6 +82,7 @@ export default function Navbar(){
             <Routes>
                 <Route path="Home" element={<Home/>}/>
                 <Route path="Resume" element={<Resume/>}/>
+                <Route path="Work" element={<Work/>}/>
             </Routes>            
         </Router>
 
