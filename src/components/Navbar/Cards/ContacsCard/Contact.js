8af6c3f613af0654c8b9afdd7840025d95f50142
LockGy <oggyyear123@gmail.com>
2023-08-21 09:13:35 +0700

    sending email

--- src/components/Navbar/Cards/ContacsCard/Contact.js
+++ src/components/Navbar/Cards/ContacsCard/Contact.js
@@ -2,8 +2,24 @@ import './Contact.css'
 import Line from './Img/Line 5.svg'
 import Phone_icon from './Img/phone.svg'
 import Email_icon from './Img/email.svg'
+import React,{useRef} from 'react'
+import emailjs from '@emailjs/browser';
 
 export default function Contacts(){
+    const form = useRef();
+       
+    const sendEmail = (e) => {
+        e.preventDefault(); // prevents the page from reloading when you hit “Send”
+       
+        emailjs.sendForm('service_ig5jphk', 'template_folaksj', form.current, 'WbqRjTx4xv6KKQ4mR')
+        .then((result) => {
+            console.log(result.text);
+                // show the user a success message
+        }, (error) => {
+            console.log(error.text);
+                // show the user an error
+        });
+    };
     return (
         <div className="Contact">
             <div className='Contact-title'>
@@ -36,6 +52,27 @@ export default function Contacts(){
                     </div>
                 </div>
             </div>
+                <div className='Contact-form'>
+                    <div className='Email-submit-text'>
+                    I am always open to discussing <strong>new projects</strong>, <strong>opportunities in tech world,
+                    partnerships</strong> and more so <strong>mentorship.</strong> 
+                    </div>
+                    
+                    <form className='Contact-input' ref={form} onSubmit={sendEmail}>
+                        <label className='Form-text'><strong>Name:</strong></label>
+                        <input className='Form-input' type="text" name="name" />
+                        <label className='Form-text'><strong>Email:</strong></label>
+                        <input className='Form-input' type="email" name="email" />
+                        <label className='Form-text'><strong>Message:</strong></label>
+                        <input className='Form-input' type='text' name="message" />
+                        <div className='Form-send'>
+                            <input  type="submit" value="Submit" />
+                        </div>
+                        
+                                                
+                    </form>
+                   
+                </div>
         </div>
     )
-}
+};
